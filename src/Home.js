2ab9b3c15fbb399037006e4d45352e73e2c56661
Halil <h.teker.developer@gmail.com>
2017-12-10 14:18:43 +0100

    Added sidebar

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Home.css';
 import PageContent from './PageContent';
+import Sidebar from './Sidebar';
+import { Container, Row, Col } from 'react-grid-system';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <PageContent>
-          Dit is de content
+          <Container>
+            <Row>
+              <Col sm={3}>
+                <Sidebar />
+              </Col>
+              <Col sm={9}>
+                Main content
+              </Col>
+            </Row>
+          </Container>
         </PageContent>
       </div>
     );
