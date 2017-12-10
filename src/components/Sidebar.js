a3bba2094d656d7e5b57814f4cd1a83ceac1f4e8
Halil <h.teker.developer@gmail.com>
2017-12-10 14:35:40 +0100

    Filled sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import '../styles/Sidebar.css';
+import CardContainer from './CardContainer';
 
 class Sidebar extends Component {
   render() {
     return (
       <div>
-      	Sidebar
+      	<CardContainer>
+      		<h3>Recently Added</h3>
+      		<ul>
+	      		<li><a href="#">Book #1</a></li>
+	      		<li><a href="#">Book #1</a></li>
+	      		<li><a href="#">Book #1</a></li>
+	      		<li><a href="#">Book #1</a></li>
+	      		<li><a href="#">Book #1</a></li>
+      		</ul>
+      	</CardContainer>
       </div>
     );
   }
