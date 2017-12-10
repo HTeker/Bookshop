0993fa0da9727511a8a5056a303de4cba56b0e07
Halil <h.teker.developer@gmail.com>
2017-12-10 16:36:08 +0100

    Simplified products and categories

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,14 @@ import CardContainer from './CardContainer';
 import ProductItem from './ProductItem';
 import Navigation from './Navigation';
 
+const products = [
+  { id: '0132350886', name: 'Clean Code : A Handbook of Agile Software Craftsmanship', price: 19.99, imgUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/1323/9780132350884.jpg'},
+  { id: '0552565970', name: 'Wonder', price: 7.30, imgUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/5525/9780552565974.jpg'},
+  { id: '0008164657', name: 'Bad Dad', price: 9.17, imgUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/0081/9780008164652.jpg'},
+  { id: '1847399304', name: 'The Bro Code', price: 8.01, imgUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/8473/9781847399304.jpg'},
+  { id: '0099519852', name: 'The Talent Code : Greatness isn\'t born. It\'s grown', price: 9.59, imgUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/0995/9780099519850.jpg'}
+];
+
 class Home extends Component {
   render() {
     return (
@@ -21,11 +29,21 @@ class Home extends Component {
             <Col md={9}>
               <CardContainer>
                 <h3>Bestselling Books</h3>
-                <ProductItem product={{ id: '0132350886', name: 'Clean Code : A Handbook of Agile Software Craftsmanship', price: 19.99, imgUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/1323/9780132350884.jpg'}} />
-                <ProductItem product={{ id: '0552565970', name: 'Wonder', price: 7.30, imgUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/5525/9780552565974.jpg'}} />
-                <ProductItem product={{ id: '0008164657', name: 'Bad Dad', price: 9.17, imgUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/0081/9780008164652.jpg'}} />
-                <ProductItem product={{ id: '1847399304', name: 'The Bro Code', price: 8.01, imgUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/8473/9781847399304.jpg'}} />
-                <ProductItem product={{ id: '0099519852', name: 'The Talent Code : Greatness isn\'t born. It\'s grown', price: 9.59, imgUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/0995/9780099519850.jpg'}} />
+                {products.map(function(product){
+                  return(<ProductItem product={product} />);
+                })}
+              </CardContainer>
+              <CardContainer>
+                <h3>New Books</h3>
+                {products.map(function(product){
+                  return(<ProductItem product={product} />);
+                })}
+              </CardContainer>
+              <CardContainer>
+                <h3>Best Rated Books</h3>
+                {products.map(function(product){
+                  return(<ProductItem product={product} />);
+                })}
               </CardContainer>
             </Col>
           </Row>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,20 +2,39 @@ import React, { Component } from 'react';
 import '../styles/Sidebar.css';
 import CardContainer from './CardContainer';
 
+const categories = [
+  {name: 'Category #1'},
+  {name: 'Category #2'},
+  {name: 'Category #3'},
+  {name: 'Category #4'},
+  {name: 'Category #5'},
+  {name: 'Category #6'},
+  {name: 'Category #7'},
+  {name: 'Category #8'},
+  {name: 'Category #9'},
+  {name: 'Category #10'}
+];
+
 class Sidebar extends Component {
   render() {
     return (
       <div>
       	<CardContainer>
-      		<h3>Recently Added</h3>
+      		<h3>Popular Categories</h3>
       		<ul>
-	      		<li><a href="#">Book #1</a></li>
-	      		<li><a href="#">Book #1</a></li>
-	      		<li><a href="#">Book #1</a></li>
-	      		<li><a href="#">Book #1</a></li>
-	      		<li><a href="#">Book #1</a></li>
+            {categories.map(function(category){
+              return(<li><a href="#">{category.name}</a></li>);
+            })}
       		</ul>
       	</CardContainer>
+        <CardContainer>
+          <h3>New Categories</h3>
+          <ul>
+            {categories.map(function(category){
+              return(<li><a href="#">{category.name}</a></li>);
+            })}
+          </ul>
+        </CardContainer>
       </div>
     );
   }
