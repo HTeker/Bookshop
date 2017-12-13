7b573bf93ddd295c1aa20ee7522aa5e05c1259af
Halil Teker <h.teker.developer>
2017-12-13 10:04:55 +0100

    Added login page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
 import '../../styles/Login.css';
+import { Row, Col } from 'react-grid-system';
 
 import Page from '../Page';
+import CardContainer from '../CardContainer';
 
 class Login extends Component {
 	render() {
 		return (
 			<Page id="login">
-				Login
+				<Row>
+					<Col md={4}>
+					</Col>
+					<Col md={4}>
+						<CardContainer>
+							<h3>Login</h3>
+							<input type="text" name="email" placeholder="E-mailaddress" className="full-width" />
+							<input type="password" name="password" placeholder="Password" className="full-width" />
+							<button className="btn primary-btn btn-full-width">Login</button>
+						</CardContainer>
+					</Col>
+					<Col md={4}>
+					</Col>
+				</Row>
+				<div className="or">OR</div>
+				<Row>
+					<Col md={4}>
+					</Col>
+					<Col md={4}>
+						<CardContainer>
+							<h3>Signup</h3>
+							<input type="text" name="name" placeholder="Name" className="full-width" />
+							<input type="text" name="email" placeholder="E-mailaddress" className="full-width" />
+							<input type="password" name="password" placeholder="Password" className="full-width" />
+							<input type="password" name="password-confirm" placeholder="Password Confirmation" className="full-width" />
+							<input type="text" name="street" placeholder="Street" className="full-width" />
+							<input type="text" name="number" placeholder="Number" className="full-width" />
+							<input type="text" name="city" placeholder="City" className="full-width" />
+							<input type="text" name="zipcode" placeholder="Zipcode" className="full-width" />
+							<button className="btn primary-btn btn-full-width">Login</button>
+						</CardContainer>
+					</Col>
+					<Col md={4}>
+					</Col>
+				</Row>
 			</Page>
 		);
 	}
