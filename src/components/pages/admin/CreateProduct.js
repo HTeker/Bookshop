4fd3daace2172b0312d60abd7767ff4c354c875c
Halil Teker <h.teker.developer>
2017-12-14 09:57:18 +0100

    Made form object in state

--- src/components/pages/admin/CreateProduct.js
+++ src/components/pages/admin/CreateProduct.js
@@ -21,13 +21,15 @@ class CreateProduct extends Component {
 			success: '',
 			error: '',
 
-			id: '',
-			name: '',
-			description: '',
-			price: 0.00,
-			imgUrl: '',
-			stock: 0,
-			deliveryDays: 0
+			form: {
+				id: '',
+				name: '',
+				description: '',
+				price: 0.00,
+				imgUrl: '',
+				stock: 0,
+				deliveryDays: 0
+			}
 		};
 
 		axios.get(config.api + '/category')
@@ -37,22 +39,12 @@ class CreateProduct extends Component {
 	}
 
 	submit() {
-		const form = {
-			id: this.state.id,
-			name: this.state.name,
-			description: this.state.description,
-			price: this.state.price,
-			imgUrl: this.state.imgUrl,
-			stock: this.state.stock,
-			deliveryDays: this.state.deliveryDays
-		};
-
 		this.setState({loading: true});
 		this.setState({success: ''});
 		this.setState({error: ''});
 		const self = this;
 
-		axios.post(config.api + '/product', form)
+		axios.post(config.api + '/product', this.state.form)
 		  .then(function (response) {
 		  	self.setState({loading: false});
 		  	self.setState({success: response.statusText});
@@ -66,8 +58,8 @@ class CreateProduct extends Component {
 	}
 
 	handleChange(name, e) {
-		var change = {};
-		change[name] = e.target.value;
+		var change = {form: this.state.form};
+		change.form[name] = e.target.value;
 		this.setState(change);
 	}
 
@@ -81,19 +73,19 @@ class CreateProduct extends Component {
 						<CardContainer>
 							<h3>Create Product</h3>
 							<label htmlFor="id">ISBN:</label>
-							<input type="text" name="id" placeholder="ISBN" className="full-width" value={this.state.id} onChange={this.handleChange.bind(this, 'id')} />
+							<input type="text" name="id" placeholder="ISBN" className="full-width" value={this.state.form.id} onChange={this.handleChange.bind(this, 'id')} />
 							<label htmlFor="name">Name:</label>
-							<input type="text" name="name" placeholder="Name" className="full-width" value={this.state.name} onChange={this.handleChange.bind(this, 'name')} />
+							<input type="text" name="name" placeholder="Name" className="full-width" value={this.state.form.name} onChange={this.handleChange.bind(this, 'name')} />
 							<label htmlFor="description">Description:</label>
-							<input type="text" name="description" placeholder="Description" className="full-width" value={this.state.description} onChange={this.handleChange.bind(this, 'description')} />
+							<input type="text" name="description" placeholder="Description" className="full-width" value={this.state.form.description} onChange={this.handleChange.bind(this, 'description')} />
 							<label htmlFor="price">Price:</label>
-							<input type="number" name="price" placeholder="Price" min="0.01" step="0.01" className="full-width" value={this.state.price} onChange={this.handleChange.bind(this, 'price')} />
+							<input type="number" name="price" placeholder="Price" min="0.01" step="0.01" className="full-width" value={this.state.form.price} onChange={this.handleChange.bind(this, 'price')} />
 							<label htmlFor="imgUrl">Image URL:</label>
-							<input type="text" name="imgUrl" placeholder="Image URL" className="full-width" value={this.state.imgUrl} onChange={this.handleChange.bind(this, 'imgUrl')} />
+							<input type="text" name="imgUrl" placeholder="Image URL" className="full-width" value={this.state.form.imgUrl} onChange={this.handleChange.bind(this, 'imgUrl')} />
 							<label htmlFor="stock">Stock:</label>
-							<input type="number" name="stock" placeholder="Stock" className="full-width" value={this.state.stock} onChange={this.handleChange.bind(this, 'stock')} />
+							<input type="number" name="stock" placeholder="Stock" className="full-width" value={this.state.form.stock} onChange={this.handleChange.bind(this, 'stock')} />
 							<label htmlFor="deliveryDays">Delivery:</label>
-							<input type="number" name="deliveryDays" placeholder="Delivery days" className="full-width" value={this.state.deliveryDays} onChange={this.handleChange.bind(this, 'deliveryDays')} />
+							<input type="number" name="deliveryDays" placeholder="Delivery days" className="full-width" value={this.state.form.deliveryDays} onChange={this.handleChange.bind(this, 'deliveryDays')} />
 
 							<LoaderAndAlert loading={this.state.loading} success={this.state.success} error={this.state.error} />
 
