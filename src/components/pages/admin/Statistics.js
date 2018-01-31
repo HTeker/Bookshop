38ff39fd4dddf696e18254110c5f8b963b420cb3
Halil Teker <h.teker.developer>
2018-01-31 16:05:41 +0100

    Added README.md

--- src/components/pages/admin/Statistics.js
+++ src/components/pages/admin/Statistics.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import Iframe from 'react-iframe';
 import { Row, Col } from 'react-grid-system';
 import axios from 'axios';
 
@@ -9,52 +11,8 @@ import CardContainer from '../../CardContainer';
 
 import {PieChart} from 'react-easy-chart';
 
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from "react-google-maps";
-
-import HeatmapLayer from "react-google-maps/lib/components/visualization/HeatmapLayer";
-
-/*const MapWithAMarker = withGoogleMap(props =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-  >
-    <HeatmapLayer defaultData={{ lat: -34.397, lng: 150.644 }} />
-  </GoogleMap>
-);*/
-
-const google = window.google;
-
 var env = process.env.NODE_ENV || 'development',
     config = require('../../../config')[env];
-
-var data = [];
-
-axios.get(config.api + '/orders', {
-		headers: { Authorization: "Bearer " + sessionStorage.getItem('token') }
-	})
-	.then(function (response) {
-		response.data.forEach(function(customer){
-			if(customer.lat && customer.lng && customer.orders.length > 0){
-				//console.log(window.google);
-				data.push(new window.google.maps.LatLng(customer.lat, customer.lng));
-			}
-		});
-	}.bind(this));
-
-const MapWithAMarker = withScriptjs(withGoogleMap(props =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{lat: 52.3702160, lng: 4.8951680}}
-  >
-    <HeatmapLayer options={{radius: 10}} data={data} />
-  </GoogleMap>
-));
-
 
 class Statistics extends Component {
 	constructor(props){
@@ -85,11 +43,10 @@ class Statistics extends Component {
 					<Col md={8}>
 						<CardContainer>
 							<h3>Heatmap of Previous Orders</h3>
-							<MapWithAMarker
-								googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBnMgPPbPeCQKL4TvKRW5Dw26wYHOE14pk&libraries=visualization"
-								loadingElement={<div style={{ height: `100%` }} />}
-							  	containerElement={<div style={{ height: `400px` }} />}
-							  	mapElement={<div style={{ height: `100%` }} />}
+							<Iframe  url="http://localhost:3000/map.html"
+								display="initial"
+        						position="relative"
+        						height="450px"
 							/>
 						</CardContainer>
 					</Col>
